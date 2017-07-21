92593e42014f737d1f6d03dd81b13a3a5d26d617
pasupol bunsaen <niawkung2@gmail.com>
2017-07-22 03:40:53 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $("button").click(function() {
         $.ajax({
             type: "GET",
-            url: "https://iapi.bot.or.th/Stat/Stat-ReferenceRate/DAILY_REF_RATE_V1/?start_period=2017-07-10&end_period=2017-07-20",
+            url: "https://iapi.bot.or.th/Stat/Stat-ReferenceRate/DAILY_REF_RATE_V1/?start_period=2017-07-10&end_period=2017-07-22",
             beforeSend: function(xhr) { xhr.setRequestHeader('api-key', 'U9G1L457H6DCugT7VmBaEacbHV9RX0PySO05cYaGsm'); },
             success: function(result) {
                 // $("#report_name_th").html(JSON.stringify(result.result.data.data_header.report_name_th));
